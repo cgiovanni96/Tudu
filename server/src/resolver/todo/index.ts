c43b5a9aa1bc7278c04d5c1ea2a5618d70e9eb25
cgiovanni96 <cgiovanni1996@gmail.com>
2020-12-04 09:00:32 +0100

    :sparkles: server: feat: todo crud operations

--- server/src/resolver/todo/index.ts
+++ server/src/resolver/todo/index.ts
@@ -1,3 +1,4 @@
+import { name } from 'faker'
 import { Arg, Mutation, Query, Resolver } from 'type-graphql'
 import Todo from '../../database/entity/Todo'
 import AddTodoInputType from './types/AddTodoInputType'
@@ -14,15 +15,47 @@ export default class TodoResolver {
 		}
 	}
 
+	@Query(() => Todo, { nullable: true })
+	todo(@Arg('name') name: string): Promise<Todo | null> {
+		try {
+			return Todo.findOneOrFail({ where: { name } })
+		} catch (e) {
+			console.error('Something went wrong ', e.message)
+			return null
+		}
+	}
+
 	@Mutation(() => Todo)
 	async addTodo(@Arg('data') newTodoData: AddTodoInputType): Promise<Todo> {
-		const { name, description, dueDate } = newTodoData
+		const { name, description, dueDate, status } = newTodoData
 		try {
-			const todo = await Todo.create({ name, description, dueDate })
+			const todo = await Todo.create({ name, description, dueDate, status })
 
 			return todo.save()
 		} catch {
 			console.log('Something went wrong')
+		}
+	}
+
+	@Mutation(() => Boolean, { nullable: true })
+	async deleteTodo(@Arg('name') name: string): Promise<boolean | null> {
+		try {
+			await Todo.delete({ name })
+			return true
+		} catch (e) {
+			console.error(e.message)
+			return false
+		}
+	}
+
+	@Mutation(() => Boolean, { nullable: true })
+	async updateTodo(@Arg('name') name: string): Promise<boolean | false> {
+		try {
+			Todo.update({ name }, {})
+			return true
+		} catch {
+			console.error('Something went wrong')
+			return false
 		}
 	}
 }
